7afc98d0593054134d45162cba3c029c414c2924
AlexTLDR <alex.badragan@protonmail.com>
2025-01-08 20:28:52 +0100

    splitting the big and medium asteroids when shoot

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,20 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
         self.rect.center = self.position
+    
+    def split(self):
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            self.kill()
+            return
+        
+        split_angle = random.uniform(20, 50)
+        velocity1 = self.velocity.rotate(split_angle) * 1.2
+        velocity2 = self.velocity.rotate(-split_angle) * 1.2
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = velocity1
+        
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2.velocity = velocity2
+        self.kill()
