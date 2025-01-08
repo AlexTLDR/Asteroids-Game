d84bf9cb60d080ff8b5f68b5bc1b3d9afb2ef09a
AlexTLDR <alex.badragan@protonmail.com>
2025-01-08 17:35:28 +0100

    moving to groups

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -18,12 +22,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)  # Add the update call here
+        for sprite in updatable:
+            sprite.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        drawable.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000  # Only call tick once per frame
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -2,25 +2,35 @@ import pygame
 from circleshape import CircleShape
 from constants import *
 
-class Player(CircleShape):
+class Player(CircleShape, pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(x, y, PLAYER_RADIUS)
+        CircleShape.__init__(self, x, y, PLAYER_RADIUS)
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface((PLAYER_RADIUS * 2, PLAYER_RADIUS * 2), pygame.SRCALPHA)
+        self.rect = self.image.get_rect()
         self.rotation = 0
 
-    # in the player class
     def triangle(self):
+        center = pygame.Vector2(self.radius, self.radius)
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        a = center + forward * self.radius
+        b = center - forward * self.radius - right
+        c = center - forward * self.radius + right
         return [a, b, c]
 
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    def draw(self):
+        self.image.fill((0,0,0,0))
+        pygame.draw.polygon(self.image, "white", self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -29,3 +39,10 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+        self.rect.center = self.position
+        self.draw()
